705fcebead04708c8277d256f895a887a6f0343c
Nabila Siregar <nabilaputrirachmania@gmail.com>
2021-07-07 20:44:32 +0800

    updating condition for Jin

--- Module-01/01-Abstraction-Encapsulation/Homework/hero.rb
+++ Module-01/01-Abstraction-Encapsulation/Homework/hero.rb
@@ -2,7 +2,7 @@ require_relative 'character'
 
 class Hero < Character
   def decrease_hitpoint(attack_damage)
-    if deflected? == true
+    if deflected? == true && @name === "Jin Sakai"
       puts "#{@name} deflects the attack."
     else
       super(attack_damage)
