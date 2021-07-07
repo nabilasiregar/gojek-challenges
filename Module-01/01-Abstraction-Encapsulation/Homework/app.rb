e9e10d64889ceafbb6b36e8e4d989863140f5931
Nabila Siregar <nabilaputrirachmania@gmail.com>
2021-07-07 11:48:18 +0800

    updating loop

--- Module-01/01-Abstraction-Encapsulation/Homework/app.rb
+++ Module-01/01-Abstraction-Encapsulation/Homework/app.rb
@@ -8,24 +8,13 @@ khotun = Hero.new("Khotun Khan", 500, 50)
 puts khotun
 puts "\n"
 
-# until khotun.is_dead? || jin.is_dead? do
-#   jin.attack(khotun)
-#   puts khotun
-#   puts "\n"
-
-#   khotun.attack(jin)
-#   puts jin
-#   puts "\n"
-# end
-
-loop do 
+until khotun.is_dead? || jin.is_dead? do
   jin.attack(khotun)
   puts khotun
   puts "\n"
-  break if khotun.is_dead?
 
   khotun.attack(jin)
   puts jin
   puts "\n"
-  break if jin.is_dead?
 end
+
