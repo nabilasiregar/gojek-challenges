8c19003d02c2ffac3544d05b6e8550eac90867e9
Nabila Siregar <nabilaputrirachmania@gmail.com>
2021-07-28 20:36:23 +0800

    adding syntax that passes the first condition

--- Module-04/02-TDD/array_transformer.rb
+++ Module-04/02-TDD/array_transformer.rb
@@ -1,11 +1,30 @@
 class ArrayTransformer
   def transform(array)
-    if array == [5, 6, 1]
-    return [5, 6, 2]
-    elsif array == [3, 9]
-      return [4, 0]
-    elsif array == [9, 9]
-      return [1, 0, 0]
-    end
+    check_size = array.size
+    iter_count = check_size - 1
+    new_array = Array.new
+    first_add = array.cycle.take(iter_count).each { |i|
+      new_array << i
+    }
+    last_add = array.last + 1
+    new_array << last_add
+    return new_array
+
+    # new_array = Array.new
+    # first_array = array.slice(number_of_iterations).join(, )
+    # new_array << first_array
+    # last_array = array.last
+    # last_array += 1
+    # new_array << last_array
+
+
+
+    # if array == [5, 6, 1]
+    # return [5, 6, 2]
+    # elsif array == [3, 9]
+    #   return [4, 0]
+    # elsif array == [9, 9]
+    #   return [1, 0, 0]
+    # end
   end
 end
