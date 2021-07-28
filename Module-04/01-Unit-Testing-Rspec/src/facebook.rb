f30e8879d99ea8250a19f3b4f176242d529c495e
Nabila Siregar <nabilaputrirachmania@gmail.com>
2021-07-28 09:54:49 +0800

    fix if statement

--- Module-04/01-Unit-Testing-Rspec/src/facebook.rb
+++ Module-04/01-Unit-Testing-Rspec/src/facebook.rb
@@ -5,12 +5,12 @@ class WLI
   end
 
   def likes
-    if names.count > 1 
-      puts "#{names[0..-2].join(', ')} and #{names.last} like this post"
+    if names.count > 2 
+      return "#{names[0..-2].join(', ')}, and #{names.last} like this post"
     elsif names.count === 2
-      puts "#{names[0]} and #{names[1]} like this post"
+      return "#{names[0]} and #{names[1]} like this post"
     elsif names.count === 1
-      puts "#{names.first} likes this post"
+      return "#{names.first} likes this post"
     else
       puts "You have no likes"
     end
