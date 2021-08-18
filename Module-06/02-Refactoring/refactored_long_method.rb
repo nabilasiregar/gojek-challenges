6bf41249aaee29629cb7fda1f48799b0113d19b7
Nabila Siregar <nabilaputrirachmania@gmail.com>
2021-08-18 19:04:28 +0800

    refactor

--- Module-06/02-Refactoring/refactored_long_method.rb
+++ Module-06/02-Refactoring/refactored_long_method.rb
@@ -1,14 +1,8 @@
-def gross_income(report)
+def get_net_income(report)
   gross_income = report.income - report.expense
-  return gross_income
-end
-
-def net_income(report, gross_income)
   net_income = report.tax.income_tax(gross_income)
-  return net_income
 end
 
-def get_total_net_income(total_net_income, net_income)
-  total_net_income = total_net_income + net_income
-  return total_net_income
+def get_total_net_income(reports)
+  reports.sum{ |report| get_net_income(report)}
 end
